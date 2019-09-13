64c9dd4d8f0ffef69243a4cc649111dceb872718
Joey-1970 <paeper@horburg.de>
2019-09-13 21:23:05 +0200

    Update module.php

--- IPS2TankerkoenigListe/module.php
+++ IPS2TankerkoenigListe/module.php
@@ -14,6 +14,7 @@
         {
             	// Diese Zeile nicht löschen.
             	parent::Create();
+		$this->ConnectParent("{66FD608F-6C67-6011-25E3-B9ED4C3E1590}");
  	    	$this->RegisterPropertyBoolean("Open", false);
 		$this->RegisterPropertyString("ApiKey", "");
 		$this->RegisterPropertyFloat("Lat", 0.0);
