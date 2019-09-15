cca8c478604df99e93a9e882afb946bbcb041170
Joey-1970 <paeper@horburg.de>
2019-09-15 17:20:31 +0200

    Update module.php

--- IPS2TankerkoenigListe/module.php
+++ IPS2TankerkoenigListe/module.php
@@ -176,13 +176,28 @@
 					$table .= '<td class="tg-611x">'.ucwords(strtolower($Stations->name)).'</td>';
 					$table .= '<td class="tg-611x">'.ucwords(strtolower($Stations->place)).'</td>';
 					If ($this->ReadPropertyBoolean("Diesel") == true) {
-						$table .= '<td class="tg-611x">'.$Stations->diesel." €".'</td>';
+						If (floatval($Stations->diesel) == $Diesel) {
+							$table .= '<td class="tg-611x"> <font color=".$ColorCode.">'.$Stations->diesel." €".'</font> </td>';
+						}
+						else {
+							$table .= '<td class="tg-611x"> <font color="white">'.$Stations->diesel." €".'</font> </td>';
+						}
 					}
 					If ($this->ReadPropertyBoolean("E5") == true) {
-						$table .= '<td class="tg-611x">'.$Stations->e5." €".'</td>';
+						If (floatval($Stations->e5) == $E5) {
+							$table .= '<td class="tg-611x"> <font color=".$ColorCode.">'.$Stations->e5." €".'</font> </td>';
+						}
+						else {
+							$table .= '<td class="tg-611x"> <font color="white">'.$Stations->e5." €".'</font> </td>';
+						}
 					}
 					If ($this->ReadPropertyBoolean("E10") == true) {
-						$table .= '<td class="tg-611x">'.$Stations->e10." €".'</td>';
+						If (floatval($Stations->e10) == $E10) {
+							$table .= '<td class="tg-611x"> <font color=".$ColorCode.">'.$Stations->e10." €".'</font> </td>';
+						}
+						else {
+							$table .= '<td class="tg-611x"> <font color="white">'.$Stations->e10." €".'</font> </td>';
+						}
 					}
 					$table .= '</tr>';
 				}
