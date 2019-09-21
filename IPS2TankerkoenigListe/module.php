5833635eaadb3d333352779843ca18ef31f90a3f
Joey-1970 <paeper@horburg.de>
2019-09-21 20:35:20 +0200

    Update module.php

--- IPS2TankerkoenigListe/module.php
+++ IPS2TankerkoenigListe/module.php
@@ -330,10 +330,7 @@
 			$this->SendDebug("ShowResult", "Fehler bei der Datenermittlung: ".utf8_encode($ResultArray->message), 0);
 			return;
 		} 
-		// Tabelle aufbauen
-		
-		
-		
+		// Tabelle aufbauen	
 		$table = '<style type="text/css">';
 		$table .= '<link rel="stylesheet" href="./.../webfront.css">';
 		$table .= "</style>";
@@ -390,18 +387,7 @@
 			SetValueString($this->GetIDForIdent("PetrolStationDetail"), $table);
 		}
 	}
-	
-	//    <iframe style="border: 1px solid black;" 
-        //src="http://www.openstreetmap.org/exportembed.html?bbox=10.676613450050354%2C53.86523244317649%2C10.680765509605408%2C53.867120905651895&layer=mapnik"
-        // height="350" width="425" 
-        // frameborder="0" marginwidth="0" marginheight="0" 
-        // scrolling="no">
-//</iframe>
-//<small><a href="http://www.openstreetmap.org/#map=18/53.86618/10.67869">Größere Karte anzeigen</a></small>
-
- 
-	    
-	    
+	   
 	private function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
 	{
 	        if (!IPS_VariableProfileExists($Name))
