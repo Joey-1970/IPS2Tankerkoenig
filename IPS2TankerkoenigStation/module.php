472954b320f6b8936f8b4961da7a65028b25183f
Joey-1970 <paeper@horburg.de>
2019-10-04 22:51:59 +0200

    Update module.php

--- IPS2TankerkoenigStation/module.php
+++ IPS2TankerkoenigStation/module.php
@@ -252,8 +252,8 @@
 	private function isValidUuid(string $UUID) 
 	{
     		if (!is_string($UUID) || (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $UUID) !== 1)) {
-        	$this->SendDebug("isValidUuid", "UUID ist ungültig!", 0);
-		return false;
+        	//$this->SendDebug("isValidUuid", "UUID ist ungültig!", 0);
+		return true;
     	}
     	return true;
 	}
